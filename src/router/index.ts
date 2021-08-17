6b346babdc610d7092f2cefde8a5c8d0dcf453b0
alaoks <493088943@qq.com>
2021-08-17 20:51:41 +0800

    feat: 加入歌单详情页面即响应路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,10 @@ const routes: Array<RouteRecordRaw> = [
     redirect: '/tabs/tab1'
   },
   {
+    path: '/songList',
+    component: ()=> import('@/views/SongList.vue')
+  },
+  {
     path: '/tabs/',
     component: Tabs,
     children: [
---
+++ src/views/SongList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+      <ion-header>
+          <ion-toolbar>
+              <ion-buttons slot="start">
+                  <ion-back-button default-href="/"></ion-back-button>
+              </ion-buttons>
+              <ion-title>歌单</ion-title>
+          </ion-toolbar>
+      </ion-header>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage,IonHeader,IonToolbar,IonButtons,IonBackButton,IonTitle } from '@ionic/vue'
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'song-list',
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonButtons,
+        IonBackButton,
+        IonTitle
+    }
+    
+})
+</script>
+
+<style>
+
+</style>
